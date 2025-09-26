898ac25c644231846ca0bf5f2479b5461ba7c75a
Kholofelo <kholofelomaria1@gmail.com>
2025-09-26 13:33:47 +0200

    Update Chapter5.hs

--- Chapter5.hs
+++ Chapter5.hs
@@ -1,22 +1,18 @@
--- HC5T1: Using applyTwice - apply a function three times
+import Data.Char (isUpper)
+
+-- HC5T1: Apply a function three times
 applyThrice :: (a -> a) -> a -> a
 applyThrice f x = f (f (f x))
 
--- Example usage
-applyTwice :: (a -> a) -> a -> a
-applyTwice f x = f (f x)
-
--- HC5T2: Filtering odd numbers from 1 to 30
+-- HC5T2: Filter odd numbers from 1 to 30
 oddsFrom1to30 :: [Int]
 oddsFrom1to30 = filter odd [1..30]
 
--- HC5T3: Checking if any word starts with uppercase
-import Data.Char (isUpper)
-
+-- HC5T3: Check if any word starts with uppercase
 anyStartsWithUpper :: [String] -> Bool
 anyStartsWithUpper words = any (\w -> not (null w) && isUpper (head w)) words
 
--- HC5T4: Rewrite using lambda
+-- HC5T4: Rewrite biggerThan10 using lambda
 biggerThan10 :: Int -> Bool
 biggerThan10 = \x -> x > 10
 
@@ -32,27 +28,26 @@ squaredEvens :: [Int] -> [Int]
 squaredEvens = filter even . map square
 
 -- HC5T7: Using $ operator
-result :: Int
-result = sum $ map (*2) $ filter (>3) [1..10]
+resultSum :: Int
+resultSum = sum $ map (*2) $ filter (>3) [1..10]
 
 -- HC5T8: Point-free style
 addFive :: Num a => a -> a
 addFive = (+5)
 
--- HC5T9: Higher-order function to apply a function twice to each list element
+-- HC5T9: Apply a function twice to each element in a list
 transformList :: (a -> a) -> [a] -> [a]
 transformList f = map (f . f)
 
--- HC5T10: Combining filter, map, and any
+-- HC5T10: Check if any squared value in a list is greater than 50
 checkAnySquaredGreaterThan50 :: [Int] -> Bool
 checkAnySquaredGreaterThan50 xs = any (>50) (map (^2) xs)
 
--- Main function demonstrating all tasks
 main :: IO ()
 main = do
     -- HC5T1
     putStrLn "HC5T1 - applyThrice (increment by 1):"
-    print $ applyThrice (+1) 0  -- Should print 3
+    print $ applyThrice (+1) 0
     putStrLn ""
 
     -- HC5T2
@@ -86,7 +81,7 @@ main = do
 
     -- HC5T7
     putStrLn "HC5T7 - Using $ operator:"
-    print result
+    print resultSum
     putStrLn ""
 
     -- HC5T8
