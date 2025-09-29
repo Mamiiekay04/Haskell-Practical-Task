4acf8a29099271c4bc135f4f8b1df264e2917f28
Kholofelo <kholofelomaria1@gmail.com>
2025-09-29 20:02:04 +0200

    Create Chapter6.hs

---
+++ Chapter6.hs
@@ -0,0 +1,31 @@
+HC6T1: Factorial of 5:
+120
+
+HC6T2: 10th Fibonacci number:
+55
+
+HC6T3: Sum of list [1,2,3,4,5]:
+15
+
+HC6T4: Product of list [1,2,3,4,5]:
+120
+
+HC6T5: Reversing list [1,2,3,4,5]:
+[5,4,3,2,1]
+
+HC6T6: Does element 3 exist in list [1,2,3,4,5]?
+True
+Does element 6 exist in list [1,2,3,4,5]?
+False
+
+HC6T7: Length of list [1,2,3,4,5]:
+5
+
+HC6T8: Filtering even numbers from [1..8]:
+[2,4,6,8]
+
+HC6T9: Doubling each element in [1,2,3]:
+[2,4,6]
+
+HC6T10: Filtering odd numbers from [1..10]:
+[1,3,5,7,9]
