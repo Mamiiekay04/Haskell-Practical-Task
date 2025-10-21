371885e7f530bada5afdc089a2ca0b66e72e12e6
Kholofelo <kholofelomaria1@gmail.com>
2025-10-21 14:10:09 +0200

    Create Chapter9.hs

---
+++ Chapter9.hs
@@ -0,0 +1,36 @@
+=== HC9T1: Entity Type Synonym ===
+("Alice","123 Blockchain Ave")
+("GridPoint",(10,20))
+
+=== HC9T2: Box a ===
+Empty
+Has 42
+
+=== HC9T3: addN ===
+Has 15
+Empty
+
+=== HC9T4: extract ===
+7
+0
+
+=== HC9T5: Shape a ===
+Circle {color = "red", radius = 2.5}
+Rectangle {color = (128,64,64), width = 3.0, height = 4.0}
+Circle color: red
+
+=== HC9T6: Recursive Tweet Type ===
+Tweet {content = "Hello Cardano!", likes = 10, comments = [Tweet {content = "Nice!", likes = 3, comments = []},Tweet {content = "Cool!", likes = 2, comments = []}]}
+
+=== HC9T7: Engagement Function ===
+10
+
+=== HC9T8: Recursive Sequence Data Type ===
+Node 1 (Node 2 (Node 3 End))
+
+=== HC9T9: Element Check in Sequence ===
+True
+False
+
+=== HC9T10: Binary Search Tree (BST) ===
+NodeBST 5 (NodeBST 1 EmptyBST (NodeBST 2 EmptyBST EmptyBST)) (NodeBST 10 (NodeBST 8 (NodeBST 7 EmptyBST EmptyBST) (NodeBST 9 EmptyBST EmptyBST)) EmptyBST)
